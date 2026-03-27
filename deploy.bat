4da20aa3844a52d6d1c312eba364fbb5d32c8750
Kenvvood <millnerpanagiota@gmail.com>
2026-03-27 17:00:56 +0800

    Fix deploy.bat encoding issue (use English)

--- deploy.bat
+++ deploy.bat
@@ -1,31 +1,30 @@
 @echo off
-chcp 65001 >nul
 echo ========================================
-echo    城诺科技 - 自动部署脚本
+echo    CPromiseCPI - Auto Deploy Script
 echo ========================================
 echo.
 
-:: 1. 复制 HTML 到 dist
-echo [1/4] 复制招商手册到 dist...
+:: 1. Copy HTML to dist
+echo [1/4] Copying HTML to dist...
 copy "手册\招商手册.html" "dist\index.html" /Y
 
-:: 2. 复制 images 到 dist
-echo [2/4] 复制图片素材到 dist...
+:: 2. Copy images to dist
+echo [2/4] Copying images to dist...
 if exist "dist\images" rmdir /S /Q "dist\images"
 xcopy "images" "dist\images" /E /I /Y
 
-:: 3. Git 提交
-echo [3/4] Git 提交...
+:: 3. Git commit
+echo [3/4] Git commit...
 git add .
 git commit -m "Update dist for deployment"
 
-:: 4. Git 推送
-echo [4/4] 推送到 GitHub...
+:: 4. Git push
+echo [4/4] Pushing to GitHub...
 git push
 
 echo.
 echo ========================================
-echo    部署完成！
-echo    Netlify 将自动检测并部署
+echo    Done!
+echo    Netlify will auto-deploy
 echo ========================================
 pause
